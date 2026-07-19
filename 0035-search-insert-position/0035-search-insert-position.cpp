2422be65ab39ba2a75a89f34157eb62115b3cdb3
Harshal Rohra <hr1674@srmist.edu.in>
2026-07-19 14:56:59 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.31%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+       int l= 0; 
+       int r= nums.size()-1;
+       int mid = l+(r-l)/2;
+
+       while(l<r){
+        if(nums[mid]==target) return mid;
+        else if (nums[mid]<target) {
+            l=mid+1;
+        }
+        else{
+            r=mid-1;
+        }
+        mid = l+(r-l)/2;
+       }
+       if(target>nums[l]){
+       return l+1;
+       }
+       else {
+        return l;
+       }
+    }
+};
