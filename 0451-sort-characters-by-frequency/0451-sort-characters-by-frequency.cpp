a5f6e121781d0ff1d14067f9a51588fa9f1e6b0a
Harshal Rohra <hr1674@srmist.edu.in>
2026-07-26 11:37:00 +0530

    Time: 2 ms (62.22%), Space: 11.3 MB (40.7%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+      unordered_map<char,int>mpp;
+      for(auto it : s) {
+        mpp[it]++;
+      }
+      priority_queue<pair<int,char>>pq;
+      for(auto it : mpp){
+        pq.push({it.second , it.first});
+      }
+
+      string ans = "";
+      while(!pq.empty()){
+        int count = pq.top().first;
+        char ch = pq.top().second;
+        pq.pop();
+
+        
+            ans.append(count,ch);
+        
+      }
+      return ans;
+    }
+};
