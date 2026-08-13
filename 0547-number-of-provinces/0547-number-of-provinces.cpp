10bf9b4e9169f5b66a864e5bdeea8ef25b9a85f8
Harshal Rohra <hr1674@srmist.edu.in>
2026-08-13 17:19:21 +0530

    Time: 0 ms (100%), Space: 20.9 MB (21.64%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -4,16 +4,15 @@ public:
         int n = isConnected.size();
 
         vector<vector<int>> adj(n);
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i != j && isConnected[i][j] == 1) {
+                if (isConnected[i][j] == 1 && i != j) {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
             }
         }
-        vector<int> vis(n, 0);
+        vector<int> vis(n,0);
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             if (!vis[i]) {
@@ -27,7 +26,7 @@ public:
         vis[node] = 1;
 
         for (auto it : adj[node]) {
-            if (!vis[it]) {
+            if (vis[it] != 1) {
                 dfs(it, adj, vis);
             }
         }
