76be8eb89077a1fe60509142e8c2cda4c2b1d40b
Harshal Rohra <hr1674@srmist.edu.in>
2026-08-15 22:32:48 +0530

    Time: 8 ms (61.2%), Space: 37.1 MB (26.27%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int drow[] = {-1 , 0 , 1, 0 };
+        int dcol[] = {0 , 1 , 0 , -1};
+        vector<vector<int>>vis(n , vector<int>(m,0));
+
+        for(int i = 0 ; i<n ; i++){
+            if(!vis[i][0] && grid[i][0]==1){
+                dfs(i , 0 , vis , drow , dcol , grid);
+            }
+            if(!vis[i][m-1] && grid[i][m-1]==1){
+                dfs(i , m-1 , vis , drow , dcol , grid);
+            }
+        }
+        for(int j = 0 ; j<m ; j++){
+            if(!vis[0][j] && grid[0][j]==1){
+                dfs(0 , j, vis , drow , dcol , grid);
+            }
+            if(!vis[n-1][j] && grid[n-1][j]==1){
+                dfs(n-1 , j , vis , drow , dcol , grid);
+            }
+        }
+        int count = 0 ;
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0 ; j<m ; j++){
+                if(!vis[i][j] && grid[i][j]==1){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    void dfs (int row , int col , vector<vector<int>>&vis , int drow[] , int dcol[] , vector<vector<int>>&grid){
+        vis [row][col]=1;
+        int n = grid.size();
+        int m = grid[0].size();
+
+        for(int i = 0 ; i<4 ; i++){
+            int nrow = drow[i]+row;
+            int ncol = dcol[i]+col;
+
+            if(nrow>=0 && nrow<n 
+            && ncol >=0 && ncol<m
+            && !vis[nrow][ncol]
+            && grid[nrow][ncol]==1){
+                dfs(nrow , ncol , vis ,drow , dcol , grid);
+            }
+        }
+    }
+
+};
