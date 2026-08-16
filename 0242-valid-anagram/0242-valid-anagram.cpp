4534870e11640e38d70c4502515f890640c0ad52
Harshal Rohra <hr1674@srmist.edu.in>
2026-08-17 02:24:51 +0530

    Time: 3 ms (45.27%), Space: 9.9 MB (11.55%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> mpp;
+        unordered_map<char,int>npp;
+
+        for(int i = 0 ; i<s.size(); i++){
+            mpp[s[i]]++;
+
+        }
+        for(int i = 0 ; i<t.size(); i++){
+            npp[t[i]]++;
+            
+        }
+        return mpp==npp;
+        
+    }
+};
