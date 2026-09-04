ad70461d99bb0094b4b52a812d91845e2d1cf99e
Harshal Rohra <hr1674@srmist.edu.in>
2026-09-04 23:45:54 +0530

    Time: 0 ms (100%), Space: 12.9 MB (13.16%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,12 +12,10 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p==NULL && q==NULL) return 1;
-        if(p==NULL || q== NULL) return 0;
-
-        return  (p->val == q->val) 
-        && isSameTree(p->left ,q->left) 
-        && isSameTree(p->right , q->right);
+        if (p==NULL && q==NULL)return true;
+        if (p==NULL || q==NULL)return false;
+        if (p->val  != q->val) return false;
+        return isSameTree(p->left , q->left) && isSameTree(p->right , q->right);
         
     }
 };
